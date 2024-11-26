ee2f92acc68231ace2c316721abf78c9536c74e0
Naup <naup96721@gmail.com>
2024-11-26 19:38:22 +0800

    rb tree

---
+++ rb_tree.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define mp std::make_pair
+#define pr std::pair<int, int>
+#define ll long long
+#define NAUP std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0)
+
+
+enum Color { RED, BLACK };
+
+struct Node {
+    int data;           // Node value
+    Color color;        // Node color (RED or BLACK)
+    Node* left;         // left node
+    Node* right;        // right node
+    Node* parent;       // parent node
+
+    // node constructor
+    Node(int data) 
+        : data(data), color(RED), left(nullptr), right(nullptr), parent(nullptr) {
+            
+        }
+};
+
+class RedBlackTree {
+    private:
+        
+    
+    public:
+    
+}
+
+
+int main() {
+    NAUP;
+
+
+}
