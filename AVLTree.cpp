77b55ac2d0a752907474558a20cdee8b7bd2d0af
堇姬 <96978334+Naupjjin@users.noreply.github.com>
2024-11-27 13:55:13 +0800

    Update AVLTree.cpp

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -2,19 +2,27 @@
 #define NAUP std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0)
 
 struct Node{
-    int value; // data
-    Node* Left; // Leaf left or right pointer
-    Node* Right; 
-    Node* Parent; // parent pointer
+    int data; // data
+    Node* left; // Leaf left or right pointer
+    Node* right; 
+    Node* parent; // parent pointer
     int height; // node height on AVL tree
 
     // constructor
-    Node(int val) : value(val), Left(nullptr), Right(nullptr), Parent(nullptr), height(0) {}
+    Node(int val) : data(val), left(nullptr), right(nullptr), parent(nullptr), height(0) {}
 };
 
 class AVLtree{
 private:
     Node* root;
+
+    // inorder traversal
+    void inorder(Node* node) {
+        if (node == nullptr) return;
+        inorder(node->left);
+        std::cout << node->data << " ";
+        inorder(node->right);
+    }
 
     void _Right_rotation(){
         
@@ -35,6 +43,37 @@ private:
 public:
     
     void InsertNode(int value){
+        Node* w_insert_node = new Node(value);
+        if(root == nullptr){
+            root = w_insert_node;
+            return;
+        }
+        else{
+            
+            Node* current = root;
+            Node* parent = nullptr;
+            
+            // find want to Insert Node's parent
+            while(current != nullptr){
+                parent = current;
+                if(value < parent->data){
+                    current = current->left;
+                }
+                else{
+                    current = current->right;
+                }
+            }
+            
+            // Insert Node
+            if(value < parent->data){
+                parent->left = w_insert_node;
+            } 
+            else{
+                parent->right = w_insert_node;
+                
+            }
+            
+        }
         
     }
     
@@ -45,12 +84,23 @@ public:
     Node* SearchNode(){
         return root;
     }
+    
+    void printInOrder() {
+        inorder(root);
+    }
 
 };
 
 int main() {
     NAUP;
     AVLtree tree;
+    
+    tree.InsertNode(2);
+    tree.InsertNode(5);
+    tree.InsertNode(4);
+    tree.InsertNode(15);
+    tree.InsertNode(7);
+    tree.printInOrder();
 
     return 0;
 }
